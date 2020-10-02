aa9130a97c64c88479a8460330ca90185a59ca68
Thiago B <thiagobc@hotmail.com>
2020-10-02 09:30:57 -0500

    ESlint installed

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Container } from './styles';
 
 const MainPage: React.FC = () => {
   return (
     <div>
-      <h1>To-Do teste</h1>
+      <h1>To-Do testea</h1>
     </div>
   )
 }
